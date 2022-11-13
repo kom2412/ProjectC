51e6b905efc117e60f1160f5c54d5951d6782ff9
Albert <kom2412@mail.ru>
2022-11-13 23:27:13 +0500

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int allEvenNumbers = 2; 
+
+if(number > 1)
+{
+    while(allEvenNumbers <= number)
+    {
+    Console.Write(allEvenNumbers + " ");
+    allEvenNumbers = allEvenNumbers + 2;
+    }
+}
